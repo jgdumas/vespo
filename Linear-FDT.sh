ae66a0bc2baa4e14af7fe165cbefab5f4ef2006f
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2023-03-16 15:31:44 +0100

    improve benchmarks output

--- Linear-FDT.sh
+++ Linear-FDT.sh
@@ -6,13 +6,34 @@ else
       NBTHREADS=$1
 fi
 
+# Number of iterations
+ITER=3
+
+# Paillier security
 SECU=2048
-ITER=11
+
+# Polynomial degrees from 2^8 to 2^16
+BEGLD=8
+ENDLD=16
+SIZLD=$((ENDLD - BEGLD + 1))
 
 LINFIL=bench_lin_vespo_P254.txt
 
+echo "##### VESPO linear benchmarks"
+echo "##### Doubling degrees from $((2**BEGLD)) to $((2**ENDLD))"
+PASSED=0
+PERCEN=0
+TOTALT=$((SIZLD * ITER))
 
-for LogDeg in {8..17}
+for (( LogDeg=$BEGLD; LogDeg<=$ENDLD; LogDeg++ ))
 do
-  OMP_NUM_THREADS=${NBTHREADS} ./vespo_bench $((2**LogDeg)) ${SECU} ${ITER} &>> ${LINFIL}
+  DEGREE=$((2**LogDeg))
+  echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m, current degree: ${DEGREE} ...\r"
+
+  # Actual benchmark:
+  OMP_NUM_THREADS=${NBTHREADS} ./vespo_bench ${DEGREE} ${SECU} ${ITER} &>> ${LINFIL}
+
+  PASSED=`grep VAUDIT ${LINFIL} | grep OK | wc -l`
+  PERCEN=$(( (100 * PASSED) / TOTALT ))
 done
+echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m                                 \n"
--- PoR-FDT.sh
+++ PoR-FDT.sh
@@ -7,16 +7,38 @@ else
 fi
 
 SECU=2048
-ITER=11
+ITER=3
 
 SQFIL=bench_sq_vespo_P254.txt
+
+echo "##### VESPO PoR benchmarks"
+echo "##### 8 degrees from 5816 to 4026778"
+
+PASSED=0
+PERCEN=0
+TOTALT=$(( (4+4) * ITER))
+
 for DEGREE in 5816 18390 58154 186093
 do
+    echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m, current degree: ${DEGREE} ...\r"
+
+    # Actual benchmark:
     OMP_NUM_THREADS=${NBTHREADS} ./vespo_bench ${DEGREE} ${SECU} ${ITER} &>> ${SQFIL}
+    PASSED=`grep VAUDIT ${SQFIL} | grep OK | wc -l`
+    PERCEN=$(( (100 * PASSED) / TOTALT ))
 done
 
 REFIL=bench_re_vespo_P254.txt
+
 for DEGREE in 5125 46551 426519 4026778
 do
+    echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m, current degree: ${DEGREE} ...\r"
+
+    # Actual benchmark:
     OMP_NUM_THREADS=${NBTHREADS} ./vespo_bench ${DEGREE} ${SECU} ${ITER} &>> ${REFIL}
+
+    PASSED=`grep VAUDIT ${SQFIL} ${REFIL} | grep OK | wc -l`
+    PERCEN=$(( (100 * PASSED) / TOTALT ))
 done
+
+echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m                                 \n"
