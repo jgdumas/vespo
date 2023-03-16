91d257c9a4d3412c790cc64561cc5444da6fd705
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2023-03-16 16:06:26 +0100

    VESPo testing range of degrees with given tasks

---
+++ FDT-range.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#####################################################
+### VESPo testing range of degrees with given tasks
+### Copyright(c) 2023 Jean-Guillaume Dumas
+#####################################################
+
+START=$1
+ENDLO=$2 
+
+if [ $# -lt 3 ]; then
+      NBTASKS=4
+else
+      NBTASKS=$3
+fi
+
+SECU=2048
+ITER=2
+
+LINFIL=/tmp/bench_range_vespo_P254.txt
+
+SIZLD=$((ENDLO - START + 1))
+PINIT=0
+if [ -e $file ]
+then
+  PINIT=`grep VAUDIT ${LINFIL} | grep OK | wc -l`
+fi
+PCURR=${PINIT}
+PASSED=$((PCURR-PINIT))
+PERCEN=0
+TOTALT=$((SIZLD * ITER))
+
+i=${START}
+while [ $i -le $ENDLO ]
+do 
+  echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m, OMP_NUM_THREADS=4 ./vespo_bench $i ${SECU} ${ITER} ${NBTASKS}\r"
+  OMP_NUM_THREADS=4 ./vespo_bench $i ${SECU} ${ITER} ${NBTASKS} &>> ${LINFIL}
+  i=$(( $i + 1 ))
+  PCURR=`grep VAUDIT ${LINFIL} | grep OK | wc -l`
+  PASSED=$((PCURR-PINIT))
+  PERCEN=$(( (100 * PASSED) / TOTALT ))
+done
+echo -ne "##### Passed: \e[32m${PERCEN}%\e[0m                                                   \n"
+
