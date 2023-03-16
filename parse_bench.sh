68272f62547e2e424e362013a1efddcf00abdbd4
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2023-03-16 15:50:34 +0100

    deviations

--- parse_bench.sh
+++ parse_bench.sh
@@ -8,5 +8,5 @@ do
 
 	egrep '(STATE|TIMING)' ${FILE} | xargs -n2 -d'\n' | awk 'BEGIN { print "Degree Setup CStore CTime STime Utime Htime" } {printf("%d %.2fs %.2fkB %.2fms %.2fs %.2fms %.2fms\n",$8,$14,$3/8192.,$18*1000.,$22,$30*1000.,$26*1000.)}'
 
-	fgrep DEVIATIO ${FILE} | sed 's/\%//g' | awk 'BEGIN {c=0;s=0} { if (c<$9) { c=$9 } ; if (s<$13) { s=$13 } } END {m=c; if (m<s) m=s; printf("%.2f%% (%f%% %f%%)\n",m,c,s)}'
+	fgrep DEVIATIO ${FILE} | sed 's/\%//g' | awk 'BEGIN {c=0;s=0} { if (c<$9) { c=$9 } ; if (s<$13) { s=$13 } } END {m=c; if (m<s) m=s; printf("Deviations %.2f%% (%f%% %f%%)\n",m,c,s)}'
 done
