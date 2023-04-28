ed98719aa131960cb5de5a304ad2b8aed00262c4
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2023-04-28 13:23:31 +0200

    upd commits

--- libvespo-auto-install.sh
+++ libvespo-auto-install.sh
@@ -13,7 +13,7 @@ fi
 
 ### VESPO git sources and commit ###
 VESPO_GITDIR=https://github.com/jgdumas/vespo.git
-VESPO_COMMIT=ad94b8e
+VESPO_COMMIT=f41684f
 
 ### RELIC git sources and commit ###
 RELIC_GITDIR=https://github.com/relic-toolkit/relic.git
