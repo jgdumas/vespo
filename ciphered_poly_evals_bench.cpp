98a4b4d655f36dff3eeccaa7c57b3474ca35a3c9
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2023-10-10 16:09:25 +0200

    parallelize outer loop

--- ciphered_poly_evals_bench.cpp
+++ ciphered_poly_evals_bench.cpp
@@ -7,9 +7,9 @@
 /****************************************************************
  * Uses libvespo https://github.com/jgdumas/vespo
  * Refzrence: "VESPo: Verified Evaluation of Secret Polynomials"
-        PETS 2023: 23rd Privacy Enhancing Technologies Symposium,
-        J-G. Dumas, A. Maignan, C. Pernet, D. S. Roche,
-        https://petsymposium.org/popets/2023/popets-2023-0085.phpV
+	PETS 2023: 23rd Privacy Enhancing Technologies Symposium,
+	J-G. Dumas, A. Maignan, C. Pernet, D. S. Roche,
+	https://petsymposium.org/popets/2023/popets-2023-0085.phpV
  ****************************************************************/
 
 #include "vespo_library.h"
@@ -19,14 +19,14 @@
 // Main
 //========================================================
 int main(int argc, char * argv[]) {
-        //argv[1]: Degree>1 of the polynomial
-        //argv[2]: modulus size in bits for Paillier
-        //argv[3]: number of experiments
-        //argv[4]: number of parallel tasks (default is number of threads)
+	//argv[1]: Degree>1 of the polynomial
+	//argv[2]: modulus size in bits for Paillier
+	//argv[3]: number of experiments
+	//argv[4]: number of parallel tasks (default is number of threads)
 
     if (argc < 1) {
-        std::cerr << "Usage: " << argv[0] << " <Polynomial degree> <Paillier bitsize> <Verification iterations> [tasks (default=threads)]" << std::endl;
-        return 1;
+	std::cerr << "Usage: " << argv[0] << " <Polynomial degree> <Paillier bitsize> <Verification iterations> [tasks (default=threads)]" << std::endl;
+	return 1;
     }
 
     std::srand(std::time(nullptr));
@@ -36,23 +36,23 @@ int main(int argc, char * argv[]) {
 #pragma omp parallel
 #pragma omp single
     {
-        numthreads = omp_get_num_threads();
+	numthreads = omp_get_num_threads();
     }
 #endif
 
 		// Polynomial degree
     const int64_t degree = std::max(0,atoi(argv[1]));
     const int64_t psize(degree+1);
-        // Paillier bitsize
+	// Paillier bitsize
     const uint64_t pailliersize = (argc>2?atoi(argv[2]):2048);
 		// Number of evaluation points
     const uint64_t nb_points = std::max(1,(argc>3?atoi(argv[3]):3));
-        // tasks
+	// tasks
     const int nb_tasks = (argc>4?atoi(argv[4]):std::min(numthreads,psize));
 
-        /********************************************************************
-         * RELIC SETUP
-         *******************************************************************/
+	/********************************************************************
+	 * RELIC SETUP
+	 *******************************************************************/
     if (core_init() != RLC_OK) {
 		core_clean();
 		return 1;
@@ -67,43 +67,43 @@ int main(int argc, char * argv[]) {
 	pc_param_print();
 	std::clog << "  security level: " << pc_param_level() << std::endl;
 
-        /********************************************************************
-         * VESPo: parameters
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: parameters
+	 *******************************************************************/
 	util_banner("VESPO:", 0);
 
     std::clog << "  MAX_ALLOC: " << VESPO_RELIC_LIMIT_MAX_ALLOC << std::endl;
     std::clog << "  SHAMIR_WD: " << RLC_MAX(8, RLC_WIDTH) << std::endl;
     std::clog << "  CLOCK TYP: " << CLOCKTYPE << std::endl;
-	std::clog << "  OMP CORES: " << numthreads << std::endl;
-	std::clog << "  NUM POINT: " << nb_points << std::endl;
-	std::clog << "  NUM TASKS: " << nb_tasks << std::endl;
+    std::clog << "  OMP CORES: " << numthreads << std::endl;
+    std::clog << "  NUM POINT: " << nb_points << std::endl;
+    std::clog << "  NUM TASKS: " << nb_tasks << std::endl;
 
 #ifdef VESPO_NOTSECURE
     std::clog << "  REUSEDPRD: " << VESPO_NOTSECURE << std::endl;
 #endif
 
-    double time_i(0.), time_r(0.), time_d(0.), time_e(0.);
-    Chrono c_step; c_step.start();
+    double time_i(0.), time_r(0.), time_m(0.);
+    std::vector<double> time_d(nb_points), time_e(nb_points);
+    Chrono c_step;
 
-        /********************************************************************
-         * VESPo: group order
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: group order
+	 *******************************************************************/
     bn_t group_mod; bn_null(group_mod); bn_new(group_mod); pc_get_ord(group_mod);
-	std::clog << "  MOD BITS : " << bn_bits(group_mod) << std::endl;
-	std::clog << "  PAIL SIZE: " << pailliersize << std::endl;
+    std::clog << "  MOD BITS : " << bn_bits(group_mod) << std::endl;
+    std::clog << "  PAIL SIZE: " << pailliersize << std::endl;
 
-        /********************************************************************
-         * VESPo: Benchmarking with a random polynomial
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: Benchmarking with a random polynomial
+	 *******************************************************************/
+    c_step.start();
+
     Polynomial<bn_t> P(degree, group_mod); P.random();
 
 #ifdef DEBUG
     std::clog << "[RANDOM P]: " << P << std::endl;
 #endif
-
-
-    c_step.start();
 
 
     paillier_pubkey_t pub;  // Paillier keys
@@ -115,142 +115,164 @@ int main(int argc, char * argv[]) {
     cp_shpe_gen( pub.rlc, prv, bn_bits( prv->a ), pailliersize);
     bn_sqr(pub.nsq, pub.rlc->crt->n); // store n^2 too
 
-        // Enciphered Polynomial, cut into 'blocks' chunks
-        //     last ones of size [ (degree+1)/Blocks ]
-        //     first ones of that size + 1
-        //     so that sum of sizes is 'degree+1'
-        //     W is the vector of these blocks
+	// Enciphered Polynomial, cut into 'blocks' chunks
+	//     last ones of size [ (degree+1)/Blocks ]
+	//     first ones of that size + 1
+	//     so that sum of sizes is 'degree+1'
+	//     W is the vector of these blocks
     std::vector<Polynomial<paillier_ciphertext_t>> W;
     const int64_t blocks = std::max(2,nb_tasks);
     const int64_t bigblocks = setup_block_chunks(W, degree, blocks, group_mod);
 
     std::clog << "[Setup Paillier mod "<< pub.rlc->crt->n << ']' << std::endl;
 
-        // parallel store W on server
+	// parallel store W on server
     encrypt_poly(W, P, pub, prv);
-        // parallel hide the polynomial
+	// parallel hide the polynomial
 
     time_i = c_step.stop();
+
     std::clog << "[TIME SETUP CIPHER P]: " << time_i << " (" <<
 #ifndef VESPO_NOTSECURE
-        psize
+	psize
 #else
-        std::min((int64_t)VESPO_NOTSECURE,psize)
+	std::min((int64_t)VESPO_NOTSECURE,psize)
 #endif
-              << " randomness)" << std::endl;
+	      << " randomness)" << std::endl;
+#ifdef DEBUG
+    std::clog << "pows_r: d°" << powsr_deg << '|' << apows_deg << std::endl;
+#endif
+
     c_step.start();
 
 		// Evaluation points
     Polynomial<bn_t> r(nb_points-1, group_mod); r.random();
 
-        // The correct polynomial evaluations
+	// The correct polynomial evaluations
     Polynomial<bn_t> Pr(nb_points-1, group_mod);
+#pragma omp parallel for shared(P,Pr,r)
     for(size_t l=0; l<nb_points; ++l)
-        P.eval(Pr[l], r[l]);
+	P.eval(Pr[l], r[l]);
 
     time_r = c_step.stop();
     std::clog << "[TIME Ref EVALUATION]: " << time_r << " (" << nb_points << " Horner evaluations)" << std::endl;
-    c_step.start();
-
-
-        // Enciphered polynomial evaluation
-    paillier_plaintext_t z;
-
-    const int64_t nbblocks(W.size());
-    const int64_t dblocks(nbblocks-1);
-
-    paillier_ciphertext_t zeta;
-    Polynomial<paillier_ciphertext_t> bzeta(dblocks, group_mod);
-
-    bn_t tmpz; bn_new(tmpz);
-    bn_t rpowsm; bn_new(rpowsm);
-    bn_t rpows; bn_new(rpows);
-
-    const int64_t step( std::ceil( (double)degree/(double)nb_tasks) );
-    const int64_t powsr_deg(std::max(W[0].degree(),step));
-    const int64_t apows_deg(std::max(powsr_deg, std::max(bigblocks,dblocks)));
-
-#ifdef DEBUG
-    std::clog << "pows_r: d°" << powsr_deg << '|' << apows_deg << std::endl;
-#endif
-
-    Polynomial<bn_t> pows_r(apows_deg,group_mod);
-
 
     size_t correct_geomprog(0), correct_evals(0);
 
+    c_step.start();
 
+#pragma omp parallel for shared(nb_points,group_mod,P,r,Pr,prv,pub,W,blocks,bigblocks,correct_evals,correct_geomprog,time_e,time_d)
     for(size_t l=0; l<nb_points; ++l) {
+	Chrono t_step; t_step.start();
 
-        geo_progression(pows_r, 0, r[l], powsr_deg, r[l], group_mod, time_r);
+	// Enciphered polynomial evaluation
+	paillier_plaintext_t z;
 
-        bn_copy(rpowsm, pows_r[W[0].degree()]);	// last power of r;
-        bn_mul(rpows, rpowsm, r[l]);			// next power of r;
-        bn_mod(rpows, rpows, group_mod);
+	const int64_t nbblocks(W.size());
+	const int64_t dblocks(nbblocks-1);
 
-        Polynomial<bn_t> revpow(step,group_mod);
-        for(int64_t i=0; i<(step+1); ++i) {
-            bn_copy(revpow[i],pows_r[step-i]);
-        }
-            // compute evaluation by blocks
-#pragma omp parallel for shared(bzeta,pub,W,pows_r)
-        for(int64_t i=0; i<nbblocks; ++i)
-                // server computation : zeta baby-steps
-            paillier_hom_dp(bzeta[i], pub, W[i], pows_r, W[i].degree());
+	paillier_ciphertext_t zeta;
+	Polynomial<paillier_ciphertext_t> bzeta(dblocks, group_mod);
 
-        // server computation : zeta giant-step
-        geo_progression(pows_r, bigblocks, rpows, dblocks, rpowsm,
-                        group_mod, time_r);
+	bn_t tmpz; bn_new(tmpz);
+	bn_t rpowsm; bn_new(rpowsm);
+	bn_t rpows; bn_new(rpows);
 
-        paillier_hom_dp(zeta, pub, bzeta, pows_r, dblocks);
+	const int64_t step( std::ceil( (double)degree/(double)nb_tasks) );
+	const int64_t powsr_deg(std::max(W[0].degree(),step));
+	const int64_t apows_deg(std::max(powsr_deg, std::max(bigblocks,dblocks)));
 
-        time_e += c_step.stop();
+	Polynomial<bn_t> pows_r(apows_deg,group_mod);
 
-            // Is the geometric progression correct ?
-        bn_mxp_dig(tmpz, r[l], W[0].degree()+1, group_mod);
-        if(bn_cmp(tmpz, rpows) == RLC_EQ) ++correct_geomprog;
+	double time_g(0.);
+	geo_progression(pows_r, 0, r[l], powsr_deg, r[l], group_mod, time_g);
 
-        c_step.start();
+	bn_copy(rpowsm, pows_r[W[0].degree()]);	// last power of r;
+	bn_mul(rpows, rpowsm, r[l]);			// next power of r;
+	bn_mod(rpows, rpows, group_mod);
 
-            // Client: Deciphering Paillier to get z
-        PAILLIER_DECIPHER(z, pub, prv, zeta);
-        bn_mod(z.m,z.m,group_mod);
+	Polynomial<bn_t> revpow(step,group_mod);
+	for(int64_t i=0; i<(step+1); ++i) {
+	    bn_copy(revpow[i],pows_r[step-i]);
+	}
+	    // compute evaluation by blocks
+	//#pragma omp parallel for shared(bzeta,pub,W,pows_r)
+	for(int64_t i=0; i<nbblocks; ++i)
+		// server computation : zeta baby-steps
+	  paillier_hom_dp(bzeta[i], pub, W[i], pows_r, W[i].degree());
+
+	// server computation : zeta giant-step
+	geo_progression(pows_r, bigblocks, rpows, dblocks, rpowsm,
+			group_mod, time_g);
+
+	paillier_hom_dp(zeta, pub, bzeta, pows_r, dblocks);
+
+	time_e[l] += t_step.stop();
+
+	    // Is the geometric progression correct ?
+	bn_mxp_dig(tmpz, r[l], W[0].degree()+1, group_mod);
+	if(bn_cmp(tmpz, rpows) == RLC_EQ) ++correct_geomprog; else { std::cerr << "\033[1;31mERROR GEO\033[0m " << l << std::endl; }
+
+	bn_free(tmpz);
+	bn_free(rpowsm);
+	bn_free(rpows);
+
+	t_step.start();
+
+	    // Client: Deciphering Paillier to get z
+	PAILLIER_DECIPHER(z, pub, prv, zeta);
+	bn_mod(z.m,z.m,group_mod);
 
 
-        time_d += c_step.stop();
+	time_d[l] += t_step.stop();
 
-        // Is the evaluation correct ?
-        if(bn_cmp(Pr[l],z.m) == RLC_EQ) ++correct_evals;
+	// Is the evaluation correct ?
+	if(bn_cmp(Pr[l],z.m) == RLC_EQ) ++correct_evals; else { std::cerr << "\033[1;31mERROR DEC\033[0m " << l << std::endl; }
     }
+
+    time_m = c_step.stop();
 
 
     if(correct_geomprog == nb_points) {
-        std::clog << "[CLEARTEXT GEOM-PROG] \033[1;32mOK\033[0m ";
+	std::clog << "[CLEARTEXT GEOM-PROG] \033[1;32mOK\033[0m ";
     } else {
-        std::cerr << "[CLEARTEXT GEOM-PROG] \033[1;31m***** FAIL *****\033[0m ";
+	std::cerr << "[CLEARTEXT GEOM-PROG] \033[1;31m***** FAIL *****\033[0m ";
     }
     std::clog << correct_geomprog << '/' << nb_points << std::endl;
 
 
     if(correct_evals == nb_points) {
-        std::clog << "[CIPHERED EVALUATION] \033[1;32mOK\033[0m ";
+	std::clog << "[CIPHERED EVALUATION] \033[1;32mOK\033[0m ";
     } else {
-        std::cerr << "[CIPHERED EVALUATION] \033[1;31m***** FAIL *****\033[0m ";
+	std::cerr << "[CIPHERED EVALUATION] \033[1;31m***** FAIL *****\033[0m ";
     }
     std::clog << correct_evals << '/' << nb_points << std::endl;
 
+    std::sort(time_e.begin(),time_e.end());
+    std::sort(time_d.begin(),time_d.end());
 
-    std::clog << "[TIME CIPHERED EVALS]: " << time_e << " (" << (nb_points*psize) << " Paillier operations on ciphers)" << std::endl;
-    std::clog << "[TIME DECIPHERING Z ]: " << time_d << " (" << nb_points << " Paillier decipherings)" << std::endl;
+    std::clog
+      << "[TIME CIPHERED EVALS]: "
+      <<  std::accumulate(time_e.begin(), time_e.end(), 0.)
+      << " CPU (" << (nb_points*psize) << " Paillier operations "
+      << " on ciphers, by " << nb_points << '*' << W.size() << " blocks)"
+      << " --> median eval: "  << (time_e[nb_points>>1])
+      << ", dev: " << mediandeviation(time_e) << '%' << std::endl;
+    std::clog
+      << "[TIME DECIPHERING Z ]: "
+      << std::accumulate(time_d.begin(), time_d.end(), 0.)
+      << " CPU (" << nb_points << " Paillier decipherings)"
+      << " --> median dec.: " << (time_d[nb_points>>1])
+      << ", dev: " << mediandeviation(time_d) << '%' << std::endl;
 
+    std::clog
+      << "[TIME PARMULTI-EVALS]=" << time_m
+      << " degree=" << degree << " points=" << nb_points << " cores=" << numthreads << std::endl;
 
-        /********************************************************************
-         * VESPo: clean-up
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: clean-up
+	 *******************************************************************/
     bn_free(group_mod);
-    bn_free(tmpz);
-    bn_free(rpowsm);
-    bn_free(rpows);
     paillier_freeprvkey(this->prv);
     paillier_freepubkey(this->pub);
 
--- vespo_bench.cpp
+++ vespo_bench.cpp
@@ -23,14 +23,14 @@
 // Main
 //========================================================
 int main(int argc, char * argv[]) {
-        //argv[1]: size of the vector (polynomial degree + 1)
-        //argv[2]: modulus size in bits for Paillier
-        //argv[3]: number of experiments
-        //argv[4]: number of parallel tasks (default is number of threads)
+	//argv[1]: size of the vector (polynomial degree + 1)
+	//argv[2]: modulus size in bits for Paillier
+	//argv[3]: number of experiments
+	//argv[4]: number of parallel tasks (default is number of threads)
 
     if (argc < 1) {
-        std::cerr << "Usage: " << argv[0] << " <Vector dimension> <Paillier bitsize> <Verification iterations> [tasks (default=threads)]" << std::endl;
-        return 1;
+	std::cerr << "Usage: " << argv[0] << " <Vector dimension> <Paillier bitsize> <Verification iterations> [tasks (default=threads)]" << std::endl;
+	return 1;
     }
 
     std::srand(std::time(nullptr));
@@ -40,22 +40,22 @@ int main(int argc, char * argv[]) {
 #pragma omp parallel
 #pragma omp single
     {
-        numthreads = omp_get_num_threads();
+	numthreads = omp_get_num_threads();
     }
 #endif
 
 		// Polynomial degree
     const int64_t degree = std::max(0,atoi(argv[1]) - 1);
-        // Paillier bitsize
+	// Paillier bitsize
     const uint64_t pailliersize = (argc>2?atoi(argv[2]):2048);
 		// Verification iterations
     const uint64_t nb_iter = (argc>3?atoi(argv[3]):3);
-        // tasks
+	// tasks
     const int nb_tasks = (argc>4?atoi(argv[4]):std::min(numthreads,degree+1));
 
-        /********************************************************************
-         * RELIC SETUP
-         *******************************************************************/
+	/********************************************************************
+	 * RELIC SETUP
+	 *******************************************************************/
     if (core_init() != RLC_OK) {
 		core_clean();
 		return 1;
@@ -72,9 +72,9 @@ int main(int argc, char * argv[]) {
     const uint64_t group_bits = pc_param_level()<<1;
 	std::clog << "  security level: " << pc_param_level() << std::endl;
 
-        /********************************************************************
-         * VESPo: parameters
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: parameters
+	 *******************************************************************/
 	util_banner("VESPO:", 0);
 
     std::clog << "  MAX_ALLOC: " << VESPO_RELIC_LIMIT_MAX_ALLOC << std::endl;
@@ -88,32 +88,32 @@ int main(int argc, char * argv[]) {
     std::clog << "  REUSEDPRD: " << VESPO_NOTSECURE << std::endl;
 #endif
 
-        /********************************************************************
-         * VESPo: group order
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: group order
+	 *******************************************************************/
     bn_t group_mod; bn_null(group_mod); bn_new(group_mod); pc_get_ord(group_mod);
 	std::clog << "  MOD BITS : " << bn_bits(group_mod) << std::endl;
 	std::clog << "  PAIL SIZE: " << pailliersize << std::endl;
 
-    std::vector<double> time_c(nb_iter), time_s(nb_iter), time_u((nb_iter*(nb_iter+1))/2);
+    std::vector<double> time_c(nb_iter), time_s(nb_iter), time_u((nb_iter*(nb_iter+1))>>1);
     double time_i=0., time_eval=0.;
 
-        /********************************************************************
-         * VESPo: Benchmarking with a random polynomial
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: Benchmarking with a random polynomial
+	 *******************************************************************/
     Polynomial<bn_t> P(degree, group_mod); P.random();
 
 #ifdef DEBUG
     std::clog << "[RANDOM P]: " << P << std::endl;
 #endif
 
-        /********************************************************************
-         * VESPo: BSetup client/server
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: BSetup client/server
+	 *******************************************************************/
     Chrono c_setup; c_setup.start();
 
     client_t client(degree);
-        // For now 2 threads for xi1 and xi2 the rest (at least 2) for Paillier
+	// For now 2 threads for xi1 and xi2 the rest (at least 2) for Paillier
     server_t server(degree, std::max(2,nb_tasks), group_mod);
 
     setup(client, server, pailliersize, P, time_eval);
@@ -125,67 +125,67 @@ int main(int argc, char * argv[]) {
     std::clog << "[CLIENT STATE]: " << client.private_size(group_bits) << " bits" << std::endl;
 #endif
 
-        /********************************************************************
-         * VESPo: Iterate nb_iter client/server audits
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: Iterate nb_iter client/server audits
+	 *******************************************************************/
     size_t updnum(0);
     bn_t r; bn_new(r);
     for(uint64_t i = 0; i < nb_iter; ++i) {
 
-            // Perform i random updates at random monomials
-        for(uint64_t j = 0; j < i; ++j) {
-            bn_rand_mod(r, group_mod);
-            update(client, server, P, r, std::rand() % (degree+1), time_u[updnum++]);
-        }
+	    // Perform i random updates at random monomials
+	for(uint64_t j = 0; j < i; ++j) {
+	    bn_rand_mod(r, group_mod);
+	    update(client, server, P, r, std::rand() % (degree+1), time_u[updnum++]);
+	}
 
-            // Evaluation point
-        bn_rand_mod(r, group_mod);
+	    // Evaluation point
+	bn_rand_mod(r, group_mod);
 
 #ifdef DEBUG
-        printf("r: "); bn_print(r); printf("\n");
+	printf("r: "); bn_print(r); printf("\n");
 #endif
 #ifdef VESPO_CHECKERS
-            // The correct polynomial evaluation
-        bn_t Pr; bn_new(Pr); P.eval(Pr, r);
+	    // The correct polynomial evaluation
+	bn_t Pr; bn_new(Pr); P.eval(Pr, r);
 #endif
 
-        paillier_plaintext_t z;
-            // Verified evaluation
-        bool pass = eval(z, client, server, r, nb_tasks, time_c[i], time_s[i]
+	paillier_plaintext_t z;
+	    // Verified evaluation
+	bool pass = eval(z, client, server, r, nb_tasks, time_c[i], time_s[i]
 #ifdef VESPO_TIMINGS
-                         , time_e[i], time_p[i]
-                         , time_ce[i], time_cg[i], time_cp[i]
+			 , time_e[i], time_p[i]
+			 , time_ce[i], time_cg[i], time_cp[i]
 #endif
-                         );
+			 );
 
-        if (pass) {
-            std::clog << "[ VAUDIT ] " << (i+1) << '/' << nb_iter << " \033[1;32mOK : EVERYTHING IS WORKING !\033[0m" << std::endl;
-        } else {
-            std::cerr << "[ VAUDIT ] " << (i+1) << '/' << nb_iter << " \033[1;31m****** FAIL there is a PROBLEM ******\033[0m" << std::endl;
-        }
+	if (pass) {
+	    std::clog << "[ VAUDIT ] " << (i+1) << '/' << nb_iter << " \033[1;32mOK : EVERYTHING IS WORKING !\033[0m" << std::endl;
+	} else {
+	    std::cerr << "[ VAUDIT ] " << (i+1) << '/' << nb_iter << " \033[1;31m****** FAIL there is a PROBLEM ******\033[0m" << std::endl;
+	}
 
 #ifdef VESPO_CHECKERS
-            // Audit has passed, but is the protocol sound?
-        if(bn_cmp(Pr,z.m) == RLC_EQ) {
-            std::clog << "[SOUNDNESS] \033[1;32mOK\033[0m" << std::endl;
-        } else {
-            std::cerr << "[SOUNDNESS] \033[1;31m****** FAIL ******\033[0m" << std::endl;
-        }
-        bn_free(Pr);
+	    // Audit has passed, but is the protocol sound?
+	if(bn_cmp(Pr,z.m) == RLC_EQ) {
+	    std::clog << "[SOUNDNESS] \033[1;32mOK\033[0m" << std::endl;
+	} else {
+	    std::cerr << "[SOUNDNESS] \033[1;31m****** FAIL ******\033[0m" << std::endl;
+	}
+	bn_free(Pr);
 #endif
     }
 
 
-        /********************************************************************
-         * VESPo: clean-up
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: clean-up
+	 *******************************************************************/
     bn_free(r);
     bn_free(group_mod);
 	core_clean();
 
-        /********************************************************************
-         * VESPo: Compute medians and deviations
-         *******************************************************************/
+	/********************************************************************
+	 * VESPo: Compute medians and deviations
+	 *******************************************************************/
     std::sort(time_c.begin(),time_c.end());
     std::sort(time_s.begin(),time_s.end());
     std::sort(time_u.begin(),time_u.end());
@@ -197,12 +197,12 @@ int main(int argc, char * argv[]) {
     std::sort(time_cg.begin(),time_cg.end());
     std::sort(time_cp.begin(),time_cp.end());
     printf("[Eval DEVI] %lu | zeta : %f%% | xi : %f%% | C-powm : %f%% | C-gsum : %f%% | C-H_dec: %f%%\n", degree+1, mediandeviation(time_e), mediandeviation(time_p), mediandeviation(time_ce), mediandeviation(time_cg), mediandeviation(time_cp));
-    printf("[Eval TIME] %lu | zeta : %f | xi : %f | C-powm : %f | C-gsum : %f | C-H_dec: %f | pure-horner : %f\n", degree+1, time_e[nb_iter/2], time_p[nb_iter/2], time_ce[nb_iter/2], time_cg[nb_iter/2], time_cp[nb_iter/2], time_eval);
+    printf("[Eval TIME] %lu | zeta : %f | xi : %f | C-powm : %f | C-gsum : %f | C-H_dec: %f | pure-horner : %f\n", degree+1, time_e[nb_iter>>1], time_p[nb_iter>>1], time_ce[nb_iter>>1], time_cg[nb_iter>>1], time_cp[nb_iter>>1], time_eval);
 #endif
 
     printf("[DEVIATIO] | %lu %lu %lu | audit-client : %f%% | audit-server : %f%%\n", degree+1, group_bits, pailliersize, mediandeviation(time_c), mediandeviation(time_s));
 
-    printf("[TIMINGS ] | %lu %lu %lu | setup : %f | audit-client : %f | audit-server : %f | pure-horner : %f | update : %f  \n=== end ===\n\n", degree+1, group_bits, pailliersize, time_i, time_c[time_c.size()/2], time_s[time_s.size()/2], time_eval, time_u[time_u.size()/2]);
+    printf("[TIMINGS ] | %lu %lu %lu | setup : %f | audit-client : %f | audit-server : %f | pure-horner : %f | update : %f  \n=== end ===\n\n", degree+1, group_bits, pailliersize, time_i, time_c[time_c.size()>>1], time_s[time_s.size()>>1], time_eval, time_u[time_u.size()>>1]);
 
 	return 0;
 }
